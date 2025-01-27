a7f4ef2dca4215f90d41f0e83c26946c2daf0cd2
Pepu06 <pedrogonzalezsoro@gmail.com>
2025-01-27 14:04:45 -0300

    fix de que se ve muy grande

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				bgDrAgua: '#030E26',
-				bgDrAguaDark: '#00091F',
-			},
-
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        bgDrAgua: "#030E26",
+        bgDrAguaDark: "#00091F",
+      },
+    },
+  },
+  plugins: [],
+};
